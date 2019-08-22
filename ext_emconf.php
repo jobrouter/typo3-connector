81d5683231fd8f7440b30c28c75323b28d517cc9
Chris Müller <chris@krue.ml>
2019-08-22 18:36:13 +0200

    [TASK] Add autoload information

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,4 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
+    'autoload' => [
+        'psr-4' => ['Brotkrueml\\JobRouterConnector\\' => 'Classes']
+    ],
 ];
