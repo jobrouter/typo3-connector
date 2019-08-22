e6e98c6dfcc8c675890b3219f4cf22f2e44861bb
Chris Müller <chris@krue.ml>
2019-08-22 18:18:41 +0200

    [TASK] Adjust version, state, category and description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'JobRouter Connector',
-    'description' => 'Define connections from TYPO3 to JobRouter systems',
-    'category' => 'plugin',
+    'description' => 'Define connections from TYPO3 to JobRouter digitisation platform',
+    'category' => 'module',
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
-    'state' => 'alpha',
-    'version' => '0.1.0',
+    'state' => 'beta',
+    'version' => '0.2.0-dev',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
