211a6da0ef6c4a3fa3ec3629c024a94a9f84d8dd
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-09-23 22:12:13 +0200

    [TASK] Use .editorconfig from TYPO3 core

--- Resources/Public/JavaScript/ConnectionCheck.js
+++ Resources/Public/JavaScript/ConnectionCheck.js
@@ -1,59 +1,59 @@
 define(['jquery'], function($) {
-    'use strict';
+  'use strict';
 
-    var ConnectionCheck = {};
+  var ConnectionCheck = {};
 
-    ConnectionCheck.init = function() {
-        window.addEventListener('load', function() {
-            var listElement = document.getElementById('jobrouter-connection-list');
+  ConnectionCheck.init = function() {
+    window.addEventListener('load', function() {
+      var listElement = document.getElementById('jobrouter-connection-list');
 
-            if (!listElement) {
-                return;
-            }
+      if (!listElement) {
+        return;
+      }
 
-            listElement.addEventListener('click', function(event) {
-                var linkElement = event.target.closest('.jobrouter-connection-check');
+      listElement.addEventListener('click', function(event) {
+        var linkElement = event.target.closest('.jobrouter-connection-check');
 
-                if (!linkElement) {
-                    return;
-                }
+        if (!linkElement) {
+          return;
+        }
 
-                event.preventDefault();
+        event.preventDefault();
 
-                ConnectionCheck.check(
-                    linkElement.dataset.connectionUid,
-                    linkElement.dataset.connectionName
-                );
-            });
-        });
+        ConnectionCheck.check(
+          linkElement.dataset.connectionUid,
+          linkElement.dataset.connectionName
+        );
+      });
+    });
+  };
+
+  ConnectionCheck.check = function(id, name) {
+    var url = top.TYPO3.settings.ajaxUrls['jobrouter_connection_check'];
+    var settings = {
+      type: 'POST',
+      data: {connectionId: +id}
     };
 
-    ConnectionCheck.check = function(id, name) {
-        var url = top.TYPO3.settings.ajaxUrls['jobrouter_connection_check'];
-        var settings = {
-            type: 'POST',
-            data: {connectionId: +id}
-        };
+    var notificationTitle = TYPO3.lang['connection_check_for'] + ' ' + name;
+    $.ajax(url, settings)
+      .done(function(data) {
+        if (data.check === 'ok') {
+          top.TYPO3.Notification.success(notificationTitle, TYPO3.lang['connection_successful'], 5);
+          return;
+        }
 
-        var notificationTitle = TYPO3.lang['connection_check_for'] + ' ' + name;
-        $.ajax(url, settings)
-            .done(function(data) {
-                if (data.check === 'ok') {
-                    top.TYPO3.Notification.success(notificationTitle, TYPO3.lang['connection_successful'], 5);
-                    return;
-                }
+        if (data.error) {
+          top.TYPO3.Notification.error(notificationTitle, data.error);
+          return;
+        }
 
-                if (data.error) {
-                    top.TYPO3.Notification.error(notificationTitle, data.error);
-                    return;
-                }
+        top.TYPO3.Notification.error(notificationTitle, 'Unknown error');
+      })
+      .fail(function(jqXhr, textStatus) {
+        top.TYPO3.Notification.error(notificationTitle, 'Unknown error (' + textStatus + ', ' + jqXhr.status + ')');
+      });
+  };
 
-                top.TYPO3.Notification.error(notificationTitle, 'Unknown error');
-            })
-            .fail(function(jqXhr, textStatus) {
-                top.TYPO3.Notification.error(notificationTitle, 'Unknown error (' + textStatus + ', ' + jqXhr.status + ')');
-            });
-    };
-
-    ConnectionCheck.init();
+  ConnectionCheck.init();
 });
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,6 @@
-CREATE TABLE tx_jobrouterconnector_domain_model_connection
-(
-    name varchar(30)  DEFAULT '' NOT NULL,
-    base_url varchar(255) DEFAULT '' NOT NULL,
-    username varchar(50)  DEFAULT '' NOT NULL,
-    password varchar(255) DEFAULT '' NOT NULL,
+CREATE TABLE tx_jobrouterconnector_domain_model_connection (
+	name varchar(30)  DEFAULT '' NOT NULL,
+	base_url varchar(255) DEFAULT '' NOT NULL,
+	username varchar(50)  DEFAULT '' NOT NULL,
+	password varchar(255) DEFAULT '' NOT NULL
 );
