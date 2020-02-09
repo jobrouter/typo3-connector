ad8899797d40c7afa66f7f6ad7da18deb7b14358
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-02-09 15:57:40 +0100

    [TASK] Add log table for usage in dependent extensions

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,3 +4,16 @@ CREATE TABLE tx_jobrouterconnector_domain_model_connection (
 	username varchar(50)  DEFAULT '' NOT NULL,
 	password varchar(255) DEFAULT '' NOT NULL
 );
+
+CREATE TABLE tx_jobrouterconnector_log (
+    uid int(11) unsigned NOT NULL AUTO_INCREMENT,
+    request_id varchar(13) DEFAULT '' NOT NULL,
+    time_micro double(16,4) NOT NULL default '0.0000',
+    component varchar(255) DEFAULT '' NOT NULL,
+    level tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    message text,
+    data text,
+
+    PRIMARY KEY (uid),
+    KEY request (request_id)
+);
