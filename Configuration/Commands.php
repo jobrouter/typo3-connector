db08e63116ecc98af8ce92d04d0c979850a8d962
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-03-16 16:02:55 +0100

    [TASK] Use dependency injection for declaring command

--- Configuration/Commands.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'jobrouter:crypt:generatekey' => [
-        'class' => \Brotkrueml\JobRouterConnector\Command\GenerateKeyCommand::class,
-        'schedulable' => false,
-    ],
-];
