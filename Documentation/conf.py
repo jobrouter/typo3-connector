2d872a2ef7577e6f5b802f8af4b63b537d003abe
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-06-03 17:31:16 +0200

    [RELEASE] Prepare version 0.10.1

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -27,7 +27,7 @@ author = u'Chris Müller'
 # The short X.Y version
 version = u'0.10'
 # The full version, including alpha/beta/rc tags
-release = u'0.10.1-dev'
+release = u'0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
