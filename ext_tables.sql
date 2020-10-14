b27def0445e72477cb3c3b3d2a4ff0fe7e3955bf
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-10-14 18:17:32 +0200

    [TASK] Remove log table
    The log table is is now available in the base extension.

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,16 +9,3 @@ CREATE TABLE tx_jobrouterconnector_domain_model_connection (
 
 	UNIQUE KEY handle (handle)
 );
-
-CREATE TABLE tx_jobrouterconnector_log (
-	uid int(11) unsigned NOT NULL AUTO_INCREMENT,
-	request_id varchar(13) DEFAULT '' NOT NULL,
-	time_micro double(16,4) NOT NULL default '0.0000',
-	component varchar(255) DEFAULT '' NOT NULL,
-	level tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	message text,
-	data text,
-
-	PRIMARY KEY (uid),
-	KEY request (request_id)
-);
