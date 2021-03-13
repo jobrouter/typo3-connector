72c9a9bd96aa7a7642a37fd951b2a9486026ba11
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-03-13 13:47:42 +0100

    [FEATURE] Add possibility to use extension in non-composer installations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+- Extension usable in non-composer installations
+
 ## [0.12.3] - 2021-02-06
 
 ### Added
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,7 @@ defined('TYPO3') || die();
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
     '@import "EXT:' . Brotkrueml\JobRouterConnector\Extension::KEY . '/Configuration/TypoScript/"'
 );
+
+if (!TYPO3\CMS\Core\Core\Environment::isComposerMode()) {
+    @include 'phar://' . TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(Brotkrueml\JobRouterConnector\Extension::KEY) . 'Resources/Private/PHP/jobrouter-client.phar/vendor/autoload.php';
+}
