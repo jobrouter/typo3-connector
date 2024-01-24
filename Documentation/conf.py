2bb524d675e652e0a4339b94b188cbb5acf78c75
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-24 11:10:15 +0100

    version: set to 3.0.0-dev

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -25,9 +25,9 @@ copyright = u'since 2019, Chris Müller'
 author = u'Chris Müller'
 
 # The short X.Y version
-version = u'2.0'
+version = u'3.0'
 # The full version, including alpha/beta/rc tags
-release = u'2.0.0'
+release = u'3.0.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
