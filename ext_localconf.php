2949fdcff8199c9097d085933045e11002cfd848
Chris Müller <coding@brotkrueml.dev>
2026-04-22 08:11:19 +0200

    Move hooks from deprecated ext_tables.php into ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,20 @@
 declare(strict_types=1);
 
 use JobRouter\AddOn\RestClient\Client\RestClient;
+use JobRouter\AddOn\Typo3Connector\Evaluation\Password;
 use JobRouter\AddOn\Typo3Connector\Extension;
+use JobRouter\AddOn\Typo3Connector\Hooks\DropObfuscatedPasswordInConnection;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
+
+// Encrypt for form field connection password
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][Password::class] = '';
+// Remove obfuscated password in connection record
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
+    = DropObfuscatedPasswordInConnection::class;
+
 
 ExtensionManagementUtility::addTypoScriptSetup(
     '@import "EXT:' . Extension::KEY . '/Configuration/TypoScript/"'
